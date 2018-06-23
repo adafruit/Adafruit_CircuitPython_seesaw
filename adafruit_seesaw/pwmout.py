e715a31de39795eaa1a4df045685cd891290b0f2
Dan Halbert <halbert@halwitz.org>
2018-06-23 16:32:50 -0400

    Add .fraction to PWMOut; redo crickit pin names

--- adafruit_seesaw/pwmout.py
+++ adafruit_seesaw/pwmout.py
@@ -44,13 +44,24 @@ class PWMOut:
 
     @property
     def duty_cycle(self):
-        """16 bit value that dictates how much of one cycle is high (1) versus low (0). 0xffff will
-           always be high, 0 will always be low and 0x7fff will be half high and then half low."""
+        """16-bit value that dictates how much of one cycle is high (1) versus low (0). 65535 (0xffff) will
+           always be high, 0 will always be low and 32767 (0x7fff) will be half high and then half low."""
         return self._dc
 
     @duty_cycle.setter
     def duty_cycle(self, value):
         if not 0 <= value <= 0xffff:
-            raise ValueError("Out of range")
+            raise ValueError("Must be 0 to 65535")
         self._seesaw.analog_write(self._pin, value)
         self._dc = value
+
+    @property
+    def fraction(self):
+        """Expresses duty_cycle as a fractional value. Ranges from 0.0-1.0."""
+        return self.duty_cycle / 65535
+
+    @fraction.setter
+    def fraction(self, value):
+        if not 0.0 <= value <= 1.0:
+            raise ValueError("Must be 0.0 to 1.0")
+        self.duty_cycle = int(value * 65535)
