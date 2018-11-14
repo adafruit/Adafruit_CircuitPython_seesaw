295f2f035bd9fd432597b0da8e02043c872ac189
dean <deanm1278@gmail.com>
2018-11-14 16:20:59 -0500

    DM: add soil sensor example

---
+++ examples/soil_simpletest.py
@@ -0,0 +1,21 @@
+import time
+
+from board import SCL, SDA
+import busio
+from micropython import const
+
+from adafruit_seesaw.seesaw import Seesaw
+
+i2c_bus = busio.I2C(SCL, SDA)
+
+ss = Seesaw(i2c_bus, addr=0x36)
+
+while(1):
+    # read moisture level through capacitive touch pad
+    touch = ss.moisture_read()
+
+    # read temperature from the temperature sensor
+    temp = ss.get_temp()
+
+    print("temp: " + str(temp) + "  moisture: " + str(touch))
+    time.sleep(1)
