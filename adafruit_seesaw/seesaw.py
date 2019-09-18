1b6ccba5cdf7d8ca46cbe5985f0986c810987ece
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-09-17 18:03:57 -0700

    Increased read delay for MiniTFT FeatherWing

--- adafruit_seesaw/seesaw.py
+++ adafruit_seesaw/seesaw.py
@@ -372,7 +372,7 @@ class Seesaw:
         self.read(reg_base, reg, ret)
         return ret[0]
 
-    def read(self, reg_base, reg, buf, delay=.003):
+    def read(self, reg_base, reg, buf, delay=.005):
         self.write(reg_base, reg)
         if self._drdy is not None:
             while self._drdy.value is False:
