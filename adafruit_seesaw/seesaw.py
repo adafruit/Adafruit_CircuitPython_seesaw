dab0f4af93a324ca704405343607cb0530f782c7
lady ada <limor@ladyada.net>
2021-10-09 13:14:28 -0400

    nu id code

--- adafruit_seesaw/seesaw.py
+++ adafruit_seesaw/seesaw.py
@@ -107,7 +107,8 @@ _NEOPIXEL_SHOW = const(0x05)
 
 _TOUCH_CHANNEL_OFFSET = const(0x10)
 
-_HW_ID_CODE = const(0x55)
+_SAMD09_HW_ID_CODE = const(0x55)
+_ATTINY8X7_HW_ID_CODE = const(0x87)
 _EEPROM_I2C_ADDR = const(0x3F)
 
 _ENCODER_STATUS = const(0x00)
@@ -148,11 +149,11 @@ class Seesaw:
 
         chip_id = self.read8(_STATUS_BASE, _STATUS_HW_ID)
 
-        if chip_id != _HW_ID_CODE:
+        if chip_id not in (_ATTINY8X7_HW_ID_CODE, _SAMD09_HW_ID_CODE):
             raise RuntimeError(
                 "Seesaw hardware ID returned (0x{:x}) is not "
-                "correct! Expected 0x{:x}. Please check your wiring.".format(
-                    chip_id, _HW_ID_CODE
+                "correct! Expected 0x{:x} or 0x{:x}. Please check your wiring.".format(
+                    chip_id, _SAMD09_HW_ID_CODE, _ATTINY8X7_HW_ID_CODE
                 )
             )
 
