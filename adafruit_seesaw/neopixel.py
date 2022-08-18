f5bc2ee742b1e8915cbcc51c29db41456a65d020
caternuson <caternuson@gmail.com>
2022-08-18 12:49:26 -0700

    black

--- adafruit_seesaw/neopixel.py
+++ adafruit_seesaw/neopixel.py
@@ -18,8 +18,9 @@ except ImportError:
     def const(x):
         return x
 
+
 ### hack to make sure this module is not placed in root CIRCUITPY/lib folder
-if __file__.split('/')[-2] != 'adafruit_seesaw':
+if __file__.split("/")[-2] != "adafruit_seesaw":
     raise ImportError("seesaw neopixel being imported from wrong location")
 
 __version__ = "0.0.0+auto.0"
